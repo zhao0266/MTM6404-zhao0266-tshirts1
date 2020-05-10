a801155b0af0d7a3460391017487695b1d43d157
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-05-10 13:23:16 -0400

    Adds starter files

---
+++ t-shirts.js
@@ -0,0 +1,101 @@
+const tshirts = [
+  {
+    title: 'Blue T-Shirt',
+    image: 'blue-t-shirt.jpg',
+    price: 7.99,
+    stock: 4,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Bright Purple T-Shirt',
+    image: 'bright-purple-t-shirt.jpg',
+    price: 5.99,
+    stock: 1,
+    nextShipment: {
+      day: 15,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Cobalt Blue T-Shirt',
+    image: 'cobalt-blue-t-shirt.jpg',
+    price: 9.99,
+    stock: 5,
+    nextShipment: {
+      day: 3,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Green T-Shirt',
+    image: 'green-t-shirt.jpg',
+    price: 6.99,
+    stock: 0,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Grey T-Shirt',
+    image: 'blue-t-shirt.jpg',
+    price: 4.99,
+    stock: 2,
+    nextShipment: {
+      day: 10,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Light Green T-Shirt',
+    image: 'light-green-t-shirt.jpg',
+    price: 7.99,
+    stock: 4,
+    nextShipment: {
+      day: 3,
+      month: 7,
+      year: 2020
+    }
+  },
+  {
+    title: 'Purple T-Shirt',
+    image: 'purple-t-shirt.jpg',
+    price: 7.99,
+    stock: 0,
+    nextShipment: {
+      day: 1,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Red T-Shirt',
+    image: 'red-t-shirt.jpg',
+    price: 6.99,
+    stock: 3,
+    nextShipment: {
+      day: 8,
+      month: 6,
+      year: 2020
+    }
+  },
+  {
+    title: 'Teal T-Shirt',
+    image: 'teal-t-shirt.jpg',
+    price: 7.99,
+    stock: 2,
+    nextShipment: {
+      day: 1,
+      month: 7,
+      year: 2020
+    }
+  }
+]
