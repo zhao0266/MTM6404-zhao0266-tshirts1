400531871308c7afb040e411d6b9336e020ab9ce
Michael Eisenbraun <=>
2020-12-31 10:55:43 -0500

    Updates file to meet assignments new requirements

--- t-shirts.js
+++ t-shirts.js
@@ -4,98 +4,62 @@ const tshirts = [
     image: 'blue-t-shirt.jpg',
     price: 7.99,
     stock: 4,
-    nextShipment: {
-      day: 1,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Bright Purple T-Shirt',
     image: 'bright-purple-t-shirt.jpg',
     price: 5.99,
     stock: 1,
-    nextShipment: {
-      day: 15,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Cobalt Blue T-Shirt',
     image: 'cobalt-blue-t-shirt.jpg',
     price: 9.99,
     stock: 5,
-    nextShipment: {
-      day: 3,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Green T-Shirt',
     image: 'green-t-shirt.jpg',
     price: 6.99,
     stock: 0,
-    nextShipment: {
-      day: 1,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Grey T-Shirt',
     image: 'blue-t-shirt.jpg',
     price: 4.99,
     stock: 2,
-    nextShipment: {
-      day: 10,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Light Green T-Shirt',
     image: 'light-green-t-shirt.jpg',
     price: 7.99,
     stock: 4,
-    nextShipment: {
-      day: 3,
-      month: 7,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Purple T-Shirt',
     image: 'purple-t-shirt.jpg',
     price: 7.99,
     stock: 0,
-    nextShipment: {
-      day: 1,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Red T-Shirt',
     image: 'red-t-shirt.jpg',
     price: 6.99,
     stock: 3,
-    nextShipment: {
-      day: 8,
-      month: 6,
-      year: 2020
-    }
+    quantity: 1
   },
   {
     title: 'Teal T-Shirt',
     image: 'teal-t-shirt.jpg',
     price: 7.99,
     stock: 2,
-    nextShipment: {
-      day: 1,
-      month: 7,
-      year: 2020
-    }
+    quantity: 1
   }
 ]
